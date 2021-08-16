696376bc41704281500030b7652f9fbc8951414f
Daniel McCarthy <danielmccarthy20@gmail.com>
2021-08-16 13:36:36 +0100

    Week 9 Error Handling, couldnt get excerise to work, will try fix it at a later time

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -15,8 +15,10 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.get('/:id/reviews', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  getMovieReviews(id).then(reviews => res.status(200).send(reviews));
-});
+    const id = parseInt(req.params.id);
+    getMovieReviews(id)
+    .then(reviews => res.status(200).send(reviews))
+    .catch((error) => next(error));
+  });
 
 export default router;
