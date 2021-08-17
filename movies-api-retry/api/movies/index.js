46141b17d36bbe4fce5dd5227b6d025c1415a475
kmlburke <46967737+KMLBurke@users.noreply.github.com>
2021-08-17 18:48:04 +0100

    Topic 10 - Update Movies Data/Route movies not showing

--- movies-api-retry/api/movies/index.js
+++ movies-api-retry/api/movies/index.js
@@ -5,17 +5,13 @@ import {
 
 const router = express.Router();
 
-router.get('/', (req, res,next) => {
-  getMovies()
-  .then(movies => res.status(200).send(movies))
-  .catch((error) => next(error));
+router.get('/', (req, res, next) => {
+  movieModel.find().then(movies => res.status(200).send(movies)).catch(next);
 });
 
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
-  getMovie(id)
-  .then(movie => res.status(200).send(movie))
-  .catch((error) => next(error));
+  movieModel.findByMovieDBId(id).then(movie => res.status(200).send(movie)).catch(next);
 });
 
 router.get('/:id/reviews', (req, res, next) => {
@@ -24,4 +20,5 @@ router.get('/:id/reviews', (req, res, next) => {
   .then(reviews => res.status(200).send(reviews))
   .catch((error) => next(error));
 });
+
 export default router;
--- movies-api-retry/index.js
+++ movies-api-retry/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import {loadUsers, loadMovies} from './seedData';
 import usersRouter from './api/users';
 import session from 'express-session';
 import passport from './authenticate';
+
 
 
 dotenv.config();
@@ -23,9 +24,11 @@ const errHandler = (err, req, res, next) => {
   
  
 };
+
 if (process.env.SEED_DB) {
   loadUsers();
-  }
+  loadMovies();
+}
 const app = express();
 
 
